8cdfadf253509d0545d8f11aa3cbd9a1824fc9ca
Leonid Astrin <astrinleonid@gmail.com>
2024-02-09 09:57:47 +0200

    requirements

--- backend.py
+++ backend.py
@@ -53,5 +53,5 @@ def process_input(chat, text):
     return chat.chat(text)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
